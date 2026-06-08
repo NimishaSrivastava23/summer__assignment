3fabb9a544ac192688a24520588b71c707819579
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-08 21:16:15 +0530

    Create nth_fibonacci.c

---
+++ folder/day4/nth_fibonacci.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n , term1=0, term2=1, next_term;   /*we  know that fibonaaci series starts with 0 and 1 so the
+                                             first term is 0 and second term is 1
+                                            and in fibonacci series next term is sum of previous two terms*/
+
+    printf("enter number of term:");
+    scanf("%d",&n);
+
+    for(int i=2;i<=n;i++){
+        
+        term1=term2;                                //for fourth term t2 becames t1 and t3 becames t2 
+        term2=next_term;
+        next_term=term1+term2;
+
+        if(i==n){
+            printf("%d",(next_term));
+        }
+    
+        
+    }
+}
