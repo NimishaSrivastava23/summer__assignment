54661923cfd27cacaea8f661d7bbdc65d21c0535
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-08 20:56:09 +0530

    Create sum.c

---
+++ folder/day-1/sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n, sum=0 ;
+    printf("enter number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        sum=sum+i;
+    }
+printf("sum is %d",sum);
+}
