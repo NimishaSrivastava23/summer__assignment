0d77756d4cef15e3996a2b8fc2b0120644e3d54d
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-08 21:03:28 +0530

    Create reverse.c

---
+++ folder/day2/reverse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n, reverse=0, remainder;
+    printf("enter number:");
+    scanf("%d",&n);
+    while(n!=0){
+        remainder=n%10;
+        reverse=reverse*10+remainder;
+        n=n/10;
+    }
+    printf("reverse is %d",reverse);
+}
