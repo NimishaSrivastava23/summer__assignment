2539d22833f78b2e1d8a7f727440ed3aa8a56da5
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-10 21:35:04 +0530

    Create patternn.c

---
+++ folder/day-10/patternn.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+
+    for (i = 1; i <= 5; i++) {
+
+        
+        for (j = 1; j <= 5 - i; j++) {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);      //for increasing number
+        }
+
+      
+        for (k = i - 1; k >= 1; k--) {
+            printf("%d", k);         // for decreasing numbers
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
