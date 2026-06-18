f194ac40bfeb0cdb8b0d62eb8bec47c2a70957b2
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-18 11:55:07 +0530

    Create union_array.c

---
+++ folder/day-17/union_array.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[50], arr2[50], unionArr[100];
+    int n1, n2, i, j, size = 0, found;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter elements of first array:\n");
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+        unionArr[size++] = arr1[i];
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements of second array:\n");
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Add only unique elements from second array
+    for(i = 0; i < n2; i++) {
+        found = 0;
+
+        for(j = 0; j < size; j++) {
+            if(arr2[i] == unionArr[j]) {
+                found = 1;
+                break;
+            }
+        }
+
+        if(found == 0) {
+            unionArr[size++] = arr2[i];
+        }
+    }
+
+    printf("Union of arrays:\n");
+    for(i = 0; i < size; i++) {
+        printf("%d ", unionArr[i]);
+    }
+
+    return 0;
+}
