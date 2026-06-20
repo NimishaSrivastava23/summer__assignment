7aa04241add8bcd0a49f2d2af432358b8372a9ea
NimishaSrivastava23 <nimishasrivastava75@gmail.com>
2026-06-20 15:28:13 +0530

    Create symmetric.c

---
+++ folder/day-20/symmetric.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, isSymmetric = 1;
+
+    printf("Enter the order of the matrix: ");
+    scanf("%d", &n);
+
+    int matrix[10][10];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if(isSymmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
